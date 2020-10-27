e04734f19665bf10d3084e8e9895c2d976ce4e43
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-27 01:50:57 +0000

    More than one way to format your error message, even custom with template literals and strings

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
  
 try {
     // Will produce a ReferenceError
-    myFunction();
+    // myFunction();
+
+    // Produce a TypeError
+    null.myFunction();
 } catch (error) {
-    // console.log(error)
-    console.log(error.message);
-    console.log(error.name);
-    console.log(error instanceof ReferenceError) // Test to see what kind of error (error) is. 
+    console.log(`${error.name}: it's null!`)
+    // console.log(error.message);
+    // console.log(error.name);
+    // console.log(error instanceof ReferenceError) // Test to see what kind of error (error) is. 
+} finally {
+    console.log('Finally runs regardless of result...')
 }
 
 console.log('Program continues...') // A positive with try catch is that we can handle errors without stopping the entire script. Very elegant way of handling errors. 
