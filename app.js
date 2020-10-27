a9afec7cf8c4ca4125e0d72c695b0af820bd9102
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-27 01:23:45 +0000

    control how what part of the error message you want to see

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+ 
 try {
     // Will produce a ReferenceError
     myFunction();
 } catch (error) {
-    console.log(error)
+    // console.log(error)
+    console.log(error.message);
+    console.log(error.name);
+    console.log(error instanceof ReferenceError) // Test to see what kind of error (error) is. 
 }
+
+console.log('Program continues...') // A positive with try catch is that we can handle errors without stopping the entire script. Very elegant way of handling errors. 
