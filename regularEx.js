a6c5d0859732c1e6cebb96843009e8f55511cf09
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-28 13:16:15 +0000

    regular expressions definition and our first function that evaluates regular expressions...specificaly eval() using index number of array in strings

---
+++ regularEx.js
@@ -0,0 +1,12 @@
+// A regular expression describes a pattern of characters - used for pattern matching or pattern searching, also commonly used for validation and pulling phone numbers, email addresses out of a body of text...pulling anything that has certain patterns.
+let re; // Initalised a variable which stands for regular expression. 
+re = /hello/; // set to the regular expression a regular expression literal- this is a regular expression.  
+// console.log(re)
+// console.log(re.source) // Will show the regular expression without forward slashes.
+
+// Functions that evaluate expressions:
+
+// exec() - Will Return results in an array if there is a match or null
+const result = re.exec('hello world') // we pass in whatever we want to match the regular expression to (in this case, hello world). 
+
+console.log(result);// matches from the index number of the first matching text or digit. Then also outputs the actual string that we are matching. 
