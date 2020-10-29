35984a41499193a2c69a43037f16014bae4b8b68
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-29 15:09:56 +0000

    match() functions works similarly to exec() which returns array or null, where we pass the regular expression into the match().

--- regularEx.js
+++ regularEx.js
@@ -2,7 +2,7 @@
 let re; // Initalised a variable which stands for regular expression.
 re = /hello/; 
 re = /hello/i; // set to the regular expression a regular expression literal- this is a regular expression.  i = case insensitive.
-re = /hello/g; // Global search - for an entire paragraph, will scan it all instances of hello, not just the first one it finds. 
+// re = /hello/g; // Global search - for an entire paragraph, will scan it all instances of hello, not just the first one it finds. 
 // console.log(re)
 // console.log(re.source) // Will show the regular expression without forward slashes.
 
@@ -19,5 +19,10 @@ re = /hello/g; // Global search - for an entire paragraph, will scan it all inst
 // console.log(result.input); // Gives us what we passed in
 
 // test() - Returns true or false if there is a match
-const result = re.test('Hello'); // case sensitive 'Hello' will be false.However we can meke our regular expression case insensititve using 'i flag' above - /hello/i;
-console.log(result);
+// const result = re.test('Hello'); // case sensitive 'Hello' will be false.However we can meke our regular expression case insensititve using 'i flag' above - /hello/i;
+// console.log(result);
+
+// match() - Return result array or null 
+const str = 'Hello There';
+const result = str.match(re); // works the opposit of exec() where we pass the string into the regular expression method.
+console.log(result)
