1b1c3116205559cf802a79c287340ef69821e4a9
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-31 00:07:49 +0000

    Metacharacter Symbols in action

--- metaChSym.js
+++ metaChSym.js
@@ -4,11 +4,16 @@ re = /hello/;
 re = /hello/i;
 
 // Metacharacter Symbols
+re = /^h/i;       // Must start with
+re = / world$/i;  // Must end with
+re = /^hello$/i;  // Must begin and end with
+re = /^h.llo$/i;  // Matches any ONE character (the dot between h and L.)
+re = /h*llo$/i;   // Matches any character 0 or more times
 
 
 
 // String to match
-const str = 'Hello World';
+const str = 'Hllo';
 
 
 // Log Results
